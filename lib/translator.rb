ec1ffb80872f69c82b1cb4fa25bec476b6e451fc
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-20 16:51:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,12 @@
 def load_library(path)
   require "yaml"
 
-  translator = YAML.load(File.read("#{path}"))
+  just_loaded_translator = YAML.load(File.read("#{path}"))
+  translator = {"get_emoticon" => {}, "get_meaning" => {}}
+  just_loaded_translator.each {|emotion, icon|
+    translator["get_meaning"][icon[1]] = emotion
+    translator["get_emoticon"][icon[0]] = icon[1]
+  }
   return translator
 end
 
