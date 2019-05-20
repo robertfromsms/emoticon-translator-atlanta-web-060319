0b2233339d5ec4b7c82251e1cbeb0e142eada74c
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-20 17:01:55 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(path)
+  require "yaml"
+
+  just_loaded_translator = YAML.load(File.read("#{path}"))
+  translator = {"get_emoticon" => {}, "get_meaning" => {}}
+  just_loaded_translator.each {|emotion, icon|
+    translator["get_meaning"][icon[1]] = emotion
+    translator["get_emoticon"][icon[0]] = icon[1]
+  }
+  return translator
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  translator = load_library(path)
+  if translator["get_emoticon"].has_key?(emoticon)
+    return translator["get_emoticon"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  translator = load_library(path)
+  if translator["get_meaning"].has_key?(emoticon)
+    return translator["get_meaning"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
---
+++ thing.rb
@@ -0,0 +1,5 @@
+require "yaml"
+
+thing = YAML.load(File.read("/home/colorful-exception-1233/emoticon-translator-atlanta-web-060319/lib/emoticons.yml"))
+
+puts thing
